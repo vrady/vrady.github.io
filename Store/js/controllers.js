45c3c51e2f9869fc04d4a9bbdd6b46f4b504a830
Dima Vrady <wiggledima@gmail.com>
2016-04-27 01:36:44 +0300

    new

--- Store/js/controllers.js
+++ Store/js/controllers.js
@@ -16,6 +16,28 @@ appleStoreControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '
         $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
             $scope.phone = data;
         });
+        $scope.tab = 1;
+        $scope.selectTab = function (secTab) {
+            this.tab = secTab;
+        };
+        $scope.isSelected = function (checkTab) {
+            return this.tab === checkTab;
+        };
+        $scope.review = {};
+        $scope.addReview = function (product) {
+            $scope.review.createdOn = Date.now();
+            phone.reviews.push($scope.review);
+            $scope.review = {};
+        };
         $('select').material_select();
+        $('ul.tabs').tabs();
     }]);
 
+appleStoreControllers.controller('ReviewController', function () {
+    this.review = {};
+    this.addReview = function (product) {
+        this.review.createdOn = Date.now();
+        product.reviews.push(this.review);
+        this.review = {};
+    }
+});
