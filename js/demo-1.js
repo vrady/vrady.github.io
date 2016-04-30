ca67e30d01c4cb4a55230860878ee1032ba1fc39
Dima Vrady <wiggledima@gmail.com>
2016-04-30 11:07:38 +0300

    new

--- js/demo-1.js
+++ js/demo-1.js
@@ -9,7 +9,7 @@
 
     function initHeader() {
         width = window.innerWidth;
-        height = window.innerHeight - 90;
+        height = window.innerHeight - 50;
         target = {x: width/2, y: height/2};
 
         largeHeader = document.getElementById('large-header');
