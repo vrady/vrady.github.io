0dd501bcb2ca29bbab820ed01cd371e45ab7d597
Dima Vrady <wiggledima@gmail.com>
2017-02-25 21:30:14 +0200

    dark theme

--- projects/cipher/app/app.js
+++ projects/cipher/app/app.js
@@ -1,7 +1,12 @@
 (() => {
   'use strict';
 
-  let app = angular.module('cryptographyApp', ['ngFileSaver', 'ngMaterial']);
+  let app = angular.module('cryptographyApp', ['ngFileSaver', 'ngMaterial'])
+  .config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+  .dark()
+  .primaryPalette('teal')
+});
 
   app.constant('Alphabet', {
     ALPHABET : `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯҐЄІЇабвгдеёжзийклмнопрстуфхцчшщъыьэюяґєії0123456789 .,!?()\n`
